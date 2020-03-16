778786adca393e6be16ac8ca6aee37b9709638cd
NoamProg <38458454+NoamProg@users.noreply.github.com>
2020-03-16 13:45:44 +0200

    create src and build folders, node modules to gitignore

---
+++ build/server.js
@@ -0,0 +1,6 @@
+"use strict";
+// function greeter(person: string) {
+//     return `Hello, ${person}`;
+// }
+// const user = [0, 1, 2];
+// document.body.textContent = greeter();
---
+++ src/server.ts
@@ -0,0 +1,7 @@
+// function greeter(person: string) {
+//     return `Hello, ${person}`;
+// }
+
+// const user = [0, 1, 2];
+
+// document.body.textContent = greeter();
