e83d3f63f7e1823cd7ec0415412830d7265dda8c
NoamProg <38458454+NoamProg@users.noreply.github.com>
2020-03-18 18:19:12 +0200

    init repository and manager person layers

---
+++ src/person/person.repository.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express';
+import {personModel} from './person.model';
+import Person from './person.interface';
+ 
+function createPerson(request: Request, response: Response) {
+  const personData: Person = request.body;
+  const createdPost = new personModel(personData);
+  createdPost.save()
+    .then(savedPost => {
+      response.send(savedPost);
+    })
+}
---
+++ src/person/person.router.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import { PersonController } from './person.controller';
+
+const personRouter : Router = Router();
+
+personRouter.get('/', PersonController.getPerson)
+personRouter.post('/', PersonController.addPerson)
+personRouter.put('/:id', PersonController.updatePerson)
+personRouter.delete('/:id', PersonController.deletePerson)
