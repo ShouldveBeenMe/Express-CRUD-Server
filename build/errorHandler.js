eed76669d20e5fb186c69c2dd03dd237e29f3d1d
NoamProg <38458454+NoamProg@users.noreply.github.com>
2020-03-24 17:02:58 +0200

    Remove build files

--- build/errorHandler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function handlerErrors(err, req, res, next) {
-    if (err.status)
-        res.status(err.status);
-    res.json(err.message);
-}
-exports.handlerErrors = handlerErrors;
-//# sourceMappingURL=errorHandler.js.map
